9e4fd5d7a6e8ba56fcec2dec2f8d3001a9a03d23
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-04-13 21:22:27 -0400

    Update SetFactory and add route

--- database/factories/SetFactory.php
+++ database/factories/SetFactory.php
@@ -7,17 +7,12 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Set>
  */
-class SetFactory extends Factory
+final class SetFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'number' => fake()->numberBetween(1, 5),
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\SetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::resource('exercise', ExerciseController::class);
+Route::resource('set', SetController::class);
