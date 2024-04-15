76bdd3ba0e9ad401b474969e70526361ff34b870
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-04-14 21:01:11 -0400

    Update set model

--- app/Models/Set.php
+++ app/Models/Set.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Set extends Model
+final class Set extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'number',
+        'weight',
+        'repetitions',
+        'duration',
+        'notes',
+    ];
+    protected $casts = [
+        'number' => 'int',
+        'weight' => 'int',
+        'repetitions' => 'int',
+        'duration' => 'int',
+    ];
 }
