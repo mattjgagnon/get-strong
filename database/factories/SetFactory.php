a3f45ccce2727a6be21ce278a5991c60d7722c1b
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-05-15 13:49:13 -0400

    Add some fields to the SetFactory

--- database/factories/SetFactory.php
+++ database/factories/SetFactory.php
@@ -13,6 +13,10 @@ final class SetFactory extends Factory
     {
         return [
             'number' => fake()->numberBetween(1, 5),
+            'weight' => fake()->numberBetween(0, 300),
+            'repetitions' => fake()->numberBetween(1, 20),
+            'duration' => fake()->numberBetween(1, 30),
+            'notes' => fake()->sentence,
         ];
     }
 }
