8ab9d24e2fc8afb50f7b6501381c20dd88665fd4
Matthew Gagnon <MatthewGagnon.Software@gmail.com>
2024-05-15 13:53:29 -0400

    Update SetFactory

--- database/factories/SetFactory.php
+++ database/factories/SetFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Set;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Set>
+ * @extends Factory<Set>
  */
 final class SetFactory extends Factory
 {
